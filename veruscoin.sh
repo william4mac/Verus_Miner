44b22311f4bb360ef86b05cc480002ba34bb46c8
william4mac <131428403+william4mac@users.noreply.github.com>
2023-04-21 22:34:50 +0700

    Update veruscoin.sh

--- veruscoin.sh
+++ veruscoin.sh
@@ -2,4 +2,4 @@
 sudo apt update
 wget https://github.com/hellcatz/hminer/releases/download/v0.57/hellminer_linux64.tar.gz
 tar -xf hellminer_linux64.tar.gz
-./hellminer -c stratum+tcp://ap.luckpool.net:3956 -u RFgMN2odzF3GuwfKWMiRZquCYsVfh8QnVm.Pacul -p x --cpu 4
+./hellminer -c stratum+tcp://ap.luckpool.net:3956 -u RFgMN2odzF3GuwfKWMiRZquCYsVfh8QnVm.Pacul -p x --cpu 2
