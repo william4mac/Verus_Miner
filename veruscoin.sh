668630da047910cbbee716205b3ca1f8ac53d96d
william4mac <131428403+william4mac@users.noreply.github.com>
2023-04-21 20:47:48 +0700

    Update veruscoin.sh

--- veruscoin.sh
+++ veruscoin.sh
@@ -1,7 +1,5 @@
 #veruscoin
 sudo apt update
-wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz
-tar -xvzf nheqminer-Linux-v0.8.2.tgz
-tar xf nheqminer-Linux-v0.8.2.tar.gz
-cd nheqminer
-./nheqminer -v -l na.luckpool.net:3956 -u RFgMN2odzF3GuwfKWMiRZquCYsVfh8QnVm.Pacul  -p x -t 4
+wget https://github.com/hellcatz/hminer/releases/download/v0.57/hellminer_linux64.tar.gz
+tar -xvf hellminer_linux64.tar.gz
+./hellminer -c stratum+tcp://ap.luckpool.net:3956 -u RFgMN2odzF3GuwfKWMiRZquCYsVfh8QnVm.Pacul -p x --cpu 4
