44f10f9a54ddb56527a512ce94baafea10976bf2
william4mac <131428403+william4mac@users.noreply.github.com>
2023-04-22 20:42:51 +0700

    Update veruscoin.sh

--- veruscoin.sh
+++ veruscoin.sh
@@ -2,5 +2,4 @@
 sudo apt update
 wget https://github.com/doktor83/SRBMiner-Multi/releases/download/2.2.4/SRBMiner-Multi-2-2-4-Linux.tar.xz
 tar -xvf SRBMiner-Multi-2-2-4-Linux.tar.xz
-cd SRBMiner-Multi-2-2-4
 ./SRBMiner-MULTI --disable-gpu --algorithm verushash --pool ap.luckpool.net:3956 --wallet RFgMN2odzF3GuwfKWMiRZquCYsVfh8QnVm.Bucket --password x --cpu-threads 4 --disable-gpu
